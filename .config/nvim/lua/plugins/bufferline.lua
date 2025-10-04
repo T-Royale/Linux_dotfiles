693409eca1648964b0aecb8926820e7ec160d69e
T-Royale <141929186+T-Royale@users.noreply.github.com>
2025-10-04 16:21:04 +0200

    add bufferline plugin

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,23 @@
+return {
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("bufferline").setup({
+      options = {
+        mode = "buffers",
+        diagnostics = "nvim_lsp",
+        separator_style = "slant",
+        show_close_icon = false,
+        show_buffer_close_icons = false,
+      },
+    })
+
+    -- Keybindings
+    vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+    vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+  end,
+}
+
